41e7fdcbfcff160cd589a0dd5d870de28a39bb6e
Victor Manuel Blanco Mancera <victor.blanco@ext.etniabarcelona.com>
2025-11-06 09:42:34 -0500

    first commit

---
+++ massAppTecnica/Model/CardInfoAPIResponse.swift
@@ -0,0 +1,8 @@
+//
+//  CardInfoAPIResponse.swift
+//  massAppTecnica
+//
+//  Created by Victor Manuel Blanco Mancera on 5/11/25.
+//
+
+import Foundation
---
+++ massAppTecnica/Services/Network/LocationManager.swift
@@ -0,0 +1,8 @@
+//
+//  LocationManager.swift
+//  massAppTecnica
+//
+//  Created by Victor Manuel Blanco Mancera on 6/11/25.
+//
+
+import Foundation
